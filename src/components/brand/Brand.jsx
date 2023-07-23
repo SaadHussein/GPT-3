fa405898b67293f101d117d2c601ce2ba0efe25a
SaadHussein <sa3dhusseein@gmail.com>
2023-07-23 03:36:18 +0300

    finish header and brand sections

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { google, atlassian, shopify, slack, dropbox } from "./import";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="Company Image" />
+      </div>
+      <div>
+        <img src={slack} alt="Company Image" />
+      </div>
+      <div>
+        <img src={atlassian} alt="Company Image" />
+      </div>
+      <div>
+        <img src={dropbox} alt="Company Image" />
+      </div>
+      <div>
+        <img src={shopify} alt="Company Image" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
